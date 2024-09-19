45330274447becdffef81c071965cb60f342b8ad
nima <niimak97@gmail.com>
2024-09-20 01:21:33 +0330

    explicit and implicit return types statement vs expression

--- src/chapter3_functions.rs
+++ src/chapter3_functions.rs
@@ -1,10 +1,18 @@
 pub fn entry() {
     greet_user("Nima Karimian");
-
+    println!("{}", implicit_return());
+    println!("{}", explicit_return());
     //
 }
 
 fn greet_user(users_name: &str) {
     println!("Greetings Dear {users_name}!");
+}
+fn implicit_return() -> &'static str {
+    "nimakarimian"
+}
+fn explicit_return() -> &'static str {
+    return "Explicit Return";
+    println!("val");
 }
 
