9e13e33f75af0fa87812cce08e4682d4e22b7d86
nima <niimak97@gmail.com>
2024-10-04 01:18:46 +0330

    comment definitions

--- src/chapter3_functions.rs
+++ src/chapter3_functions.rs
@@ -5,8 +5,15 @@ pub fn entry() {
     //
 }
 
+///this is a documentation comment
 fn greet_user(users_name: &str) {
+    //this is a comment
     println!("Greetings Dear {users_name}!");
+    /*this
+        is
+        a multi-line
+        comment
+        */
 }
 fn implicit_return() -> &'static str {
     "nimakarimian"
