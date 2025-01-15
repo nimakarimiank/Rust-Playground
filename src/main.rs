70102d4a63d61f92078672b96c90b842ba9f88e0
nima <niimak97@gmail.com>
2025-01-15 20:42:07 +0330

    conditions

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,22 @@
 fn main() {
 let mut tuple_data_type : (u8,&str,i32);
     tuple_data_type = (12,"nima karimian",12313);
-    println!(
-        "{:#?}", tuple_data_type
-    );
+    // println!(
+    //     "{:#?}", tuple_data_type
+    // );
+    // let full_name = tuple_data_type.1;
+    // println!("{full_name}");
+    // array types ⬇️
+    // let array_of_numbers = [1,2,34,5,6,3,23,41,123];
+    //[i32;9] indicates 9 number in an array and each has a type of i32
+    // let numbers = [[4;10];5];
+    // println!("{:#?}",numbers);
+    //
+
+    let boolean_value = true;
+    if (boolean_value) {
+        println!("we can use parenthesis in conditional statements");
+    }
 }
 
 
