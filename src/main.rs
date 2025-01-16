7b3cfad893b226311bb05b71b2a3d286db94bd72
nima <niimak97@gmail.com>
2025-01-16 17:43:40 +0330

    structs chapter 5.1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    let greetings= String::from("hello world");
-    let hello = &greetings[0..5];
-    println!("{hello}");
-    let string_literals = "nima";
-
+    let admin = Users {
+        is_active: true,
+        email: String::from("niimak97@gmail.com"),
+        password: String::from("MUNINTHINKNUMJUICE"),
+        user_name: String::from("Admin"),
+        full_name: String::from("nima karimian"),
+    };
+    println!("{}",admin.email);
 }
 
 
+struct Users {
+    is_active: bool,
+    user_name: String,
+    email: String,
+    password: String,
+    full_name: String,
+}
