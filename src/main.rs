ab01cdb360f2b8a1158f3dfa62a73c1eb7a132c5
nima <niimak97@gmail.com>
2025-01-17 16:44:01 +0330

    calculate area of rectangle v3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    let rect1 = (32u32,45u32);
-    println!("The area of rectangle is {} square pixels",area(rect1));
+    let rect1 = Rectangle {
+        height: 32,
+        width: 42,
+    };
+    //here rec1 is moved to area and is not valid from now on.
+    let area = area(rect1);
+    //rec1 is not valid since we are borrowing it here and it has been moved in ln 7
+    println!("{:#?}",rect1);
+    println!("The area of rectangle is {} square pixels", area);
 }
 
-    fn area(dimensions:(u32,u32)) -> u32 {
-        dimensions.0*dimensions.1
-    }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.height * rectangle.width
+}
