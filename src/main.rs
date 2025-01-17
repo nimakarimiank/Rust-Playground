eac073e355e5fc2ac7270c330428fcd700623c8e
nima <niimak97@gmail.com>
2025-01-17 17:28:38 +0330

    associated functions

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
+    let square = Rectangle::square(32);
+    let square_area = square.area();
+    println!("squares area is {:#?}",square_area);
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 #[derive(Debug)]
@@ -22,6 +11,12 @@ struct Rectangle {
     height: u32,
 }
 impl Rectangle {
+    fn square(size: u32)->Self{
+        Self {
+            height: size,
+            width: size
+        }
+    }
     fn area(&self)-> u32{
         self.height*self.width
     }
